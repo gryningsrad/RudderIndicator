eeb9a0b1b2d3e65cefef8cdc124a68defdd1bb16
gryningsrad <35922880+gryningsrad@users.noreply.github.com>
2018-08-22 11:54:30 +0200

    Create ShiftRegister595.c

---
+++ ShiftRegister595.c
@@ -0,0 +1,28 @@
+#include "ShiftRegister595.h"
+
+//=================================
+// Functions
+
+// Output clockpulse to ST_CL
+void ST_CL_Pulse()
+{
+  
+}
+
+// Output clockpulse to SH_CL
+void SH_CL_Pulse()
+{
+
+}
+
+// Function to set port high/low
+private void SetPortHigh(int port)
+{
+
+}
+
+private void SetPortLow(int port)
+{
+
+}
+
