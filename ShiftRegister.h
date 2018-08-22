1f6ab4ec172fa4a8f8b9cb4546625c9bd4d85d98
gryningsrad <35922880+gryningsrad@users.noreply.github.com>
2018-08-22 12:59:12 +0200

    Update ShiftRegister.h

--- ShiftRegister.h
+++ ShiftRegister.h
@@ -29,7 +29,7 @@ class ShiftReg595
   int SH_CP;
   int MR;
   // Functions
-  ShiftReg595(int DS, int OE, int ST_CP, int SH_CP, int MR);
+  ShiftRegister595::ShiftRegister(uint8_t P_DS, uint8_t _DS, uint8_t P_OE, uint8_t _OE, uint8_t P_ST_CP, uint8_t _ST_CP, uint8_t P_SH_CP, uint8_t _SH_CP, uint8_t P_MR, uint8_t _MR)
   void ST_CL_Pulse(); // Storage Clock
   void SH_CL_Pulse(); // Shift to output register
   void MR(int status); // MasterReset function 0=low, 1=high
