78e875807f3bc64fbc64c109fa48fc28fdfeca93
gryningsrad <35922880+gryningsrad@users.noreply.github.com>
2018-08-22 11:53:50 +0200

    Update ShiftRegister.h

--- ShiftRegister.h
+++ ShiftRegister.h
@@ -18,41 +18,25 @@ class ShiftReg595;
 // The class
 class ShiftReg595
 {
+  private:
+  void SetPortHight(int port);
+  void SetPortLow(int port);
   public:
-  // Ports
+  // Ports numbers
   int DS;
   int OE;
   int ST_CP;
   int SH_CP;
   int MR;
+  // Functions
+  ShiftReg595(int DS, int OE, int ST_CP, int SH_CP, int MR);
+  void ST_CL_Pulse(); // Storage Clock
+  void SH_CL_Pulse(); // Shift to output register
+  void MR(int status); // MasterReset function 0=low, 1=high
+  void OE(int status); // Enable Output, 0=low, 1=high
+  int WriteData(uint8 data); // Write 8bits of data
 }
 
-
-//=================================
-// Functions
-
-// Output clockpulse to ST_CL
-void ST_CL_Pulse()
-{
-  
-}
-
-// Output clockpulse to SH_CL
-void SH_CL_Pulse()
-{
-
-}
-
-// Function to set port high/low
-private void SetPortHigh(int port)
-{
-
-}
-
-private void SetPortLow(int port)
-{
-
-}
 
 
 #endif
